da6ef09a38e372784aa4a9b3da30e69fb2cfbcf2
Christian <bolster.christian@gmail.com>
2011-10-04 13:38:57 +0200

    bubblesort initial layout implemented

--- src/AirportScheduler/Model/Algorithm.h
+++ src/AirportScheduler/Model/Algorithm.h
@@ -9,10 +9,13 @@
 #define ALGORITHM_H_
 
 #include "Plane.h"
+#include <vector>
 
 class Algorithm
 {
 public:
+	virtual std::vector<Plane>* schedule(std::vector<Plane>* planes) = 0;
+	virtual ~Algorithm();
 };
 
 #endif /* ALGORITHM_H_ */
---
+++ src/AirportScheduler/Model/SortAlgorithms/BubbleSortAlgorithm.cpp
@@ -0,0 +1,12 @@
+/*
+ * BubbleSortAlgorithm.cpp
+ *
+ *  Created on: Oct 4, 2011
+ *      Author: christian
+ */
+
+#include "BubbleSortAlgorithm.h"
+
+std::vector<Plane>* BubbleSortAlgorithm::schedule(std::vector<Plane>* planes) {
+
+}
