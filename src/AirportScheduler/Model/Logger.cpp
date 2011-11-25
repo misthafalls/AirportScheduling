6b110b1e5fbfe42d5face0832f45b1dd0d095cf8
Christian <bolster.christian@gmail.com>
2011-11-25 13:04:29 +0100

    Logger now uses print_debug to stop printing useless info when in live version. Output of planes have to be reimplemented to cout, instead of using Logger!

--- src/AirportScheduler/Model/Logger.cpp
+++ src/AirportScheduler/Model/Logger.cpp
@@ -7,17 +7,24 @@
 
 #include "Logger.h"
 
+#define PRINT_DEBUG 0
+
 #include <time.h>
 #include <stdarg.h>
-#include <stdio.h>
 #include <iostream>
+#if PRINT_DEBUG
+#include <stdio.h>
+#endif
 using namespace std;
 
 void Logger::log(string txt) {
+#if PRINT_DEBUG
 	cout << txt << endl;
+#endif
 }
 
 void Logger::logex(const char * fmt, ...) {
+#if PRINT_DEBUG
 	va_list vl;
 
 	char msg[2048];
@@ -27,9 +34,10 @@ void Logger::logex(const char * fmt, ...) {
 	va_end(vl);
 
 	log(msg);
+#endif
 }
 
 void Logger::wait() {
 	cin.get();
-	cin.get();
+	//cin.get();
 }
