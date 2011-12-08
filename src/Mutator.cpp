add54fdf95d3215936858aa19a602c99cbbc3721
dustin <dustinbalentina@gmail.com>
2011-12-08 17:17:10 +0100

    Fixed mutator to work properly

--- src/Mutator.cpp
+++ src/Mutator.cpp
@@ -14,29 +14,16 @@ void Mutator::mutateGenomes(Genome **genomes, int size, double mutationRate) {
 
 void Mutator::mutateGenome(Genome *g, double mutationRate) {
 	srand ( time(NULL) );
-	double mutation;
-	int size = g->m_size;
+	double mutationLength = 2 * mutationRate;
+	double mutationStart = 1 - mutationRate;
 
-	for(int i = 0; i < size; i++) {
-		//randomize to chance of a mutation occurring
-		double toMutate = rand();
-		if(toMutate > 0.5) {
-			//randomize random number between 0 and mutationRate
-			mutation = rand() % mutationRate;
-			//set correct amount of mutation
-			if(toMutate > 0.75) {
-				mutation = 1 + mutation;
-			} else {
-				mutation = 1 - mutation;
-			}
-			//get the next Gene
-			Genome::Gene *gen = g->get_gene(i);
-			//get time of next Gene in minutes
-			int time = gen.getTime()->getTimeInMinutes();
-			//calculate mutated time
-			int mutatedTime = (int) (((double) time) * mutation);
-			//set mutated time to Gene
-			gen.setMinutes(mutatedTime - time);
-		}
+	for(int i = 0; i < g->get_size(); i++) {
+		mutationRate = ((double)rand())/MAX_RAND * mutationLength + mutationStart;
+
+		Genome::Gene * gen = g->get_gene(i);
+		Time time = gen->getTime();
+		time.addMinute(time.getTimeInMinutes() * mutationRate - time.getTimeInMinutes());
+
+		gen->setTime(time);
 	}
 }
