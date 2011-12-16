a2118488b713e86956433919abefcee7fb032c2f
Christian <bolster.christian@gmail.com>
2011-12-17 00:01:22 +0100

    Definetly fixed Time issues now!

--- src/Time.cpp
+++ src/Time.cpp
@@ -1,202 +1,188 @@
 /*
-* Time.cpp
-*
-* Created on: Oct 11, 2011
-* Author: SANDER
-*/
+ * Time.cpp
+ *
+ * Created on: Oct 11, 2011
+ * Author: SANDER
+ */
 
 #include "Time.h"
 
 #include <stdlib.h>
 #include <sstream>
+#include <iostream>
 
 using namespace std;
 
 Time::Time() {
-this->day = 0;
-this->hour = 0;
-this->minute = 0;
+	this->day = 0;
+	this->hour = 0;
+	this->minute = 0;
 }
 
 Time::Time(const Time & c) {
-this->day = c.day;
-this->hour = c.hour;
-this->minute = c.minute;
+	this->day = c.day;
+	this->hour = c.hour;
+	this->minute = c.minute;
 }
 
 Time::Time(std::string timeFormatted) {
-string dayString = "0";
-string hourString = "0";
-string minuteString = "0";
+	string dayString = "0";
+	string hourString = "0";
+	string minuteString = "0";
 
 //HH:MM format?
-if(timeFormatted[0] != '[') {
-bool bFoundSeperator = false;
-for(unsigned int i = 0; i < timeFormatted.length(); i++) {
-const char c = timeFormatted[i];
+	if (timeFormatted[0] != '[') {
+		bool bFoundSeperator = false;
+		for (unsigned int i = 0; i < timeFormatted.length(); i++) {
+			const char c = timeFormatted[i];
 
-if(c == ':') {
-bFoundSeperator = true;
-continue;
+			if (c == ':') {
+				bFoundSeperator = true;
+				continue;
+			}
+
+			if (bFoundSeperator) {
+				minuteString.append(1, c);
+			} else {
+				hourString.append(1, c);
+			}
+		}
+	} else {
+		int seperatorNumber = 0;
+		for (unsigned int i = 0; i < timeFormatted.length(); i++) {
+			const char c = timeFormatted[i];
+
+			if (c == ':' || c == ']') {
+				seperatorNumber++;
+				continue;
+			}
+
+			if (c == '[') {
+				continue;
+			}
+
+			if (seperatorNumber == 0) {
+				dayString.append(1, c);
+			} else if (seperatorNumber == 1) {
+				hourString.append(1, c);
+			} else {
+				minuteString.append(1, c);
+			}
+		}
+	}
+
+	this->day = atoi(dayString.c_str());
+	this->hour = atoi(hourString.c_str());
+	this->minute = atoi(minuteString.c_str());
 }
 
-if(bFoundSeperator) {
-minuteString.append(1, c);
-} else {
-hourString.append(1, c);
-}
-}
-} else {
-int seperatorNumber = 0;
-for(unsigned int i = 0; i < timeFormatted.length(); i++) {
-const char c = timeFormatted[i];
-
-if(c == ':' || c == ']') {
-seperatorNumber++;
-continue;
+Time::Time(int day, int hour, int minute) {
+	this->day = day;
+	this->hour = hour;
+	this->minute = minute;
 }
 
-if(c == '[') {
-continue;
+int Time::addDay(int days) {
+	changeTime(days * 1440);
+
+	return day;
 }
 
-if(seperatorNumber == 0) {
-dayString.append(1, c);
-} else if(seperatorNumber == 1) {
-hourString.append(1, c);
-} else {
-minuteString.append(1, c);
-}
-}
+int Time::addHour(int hours) {
+	changeTime(hours * 60);
+
+	return hour;
 }
 
-this->day = atoi(dayString.c_str());
-this->hour = atoi(hourString.c_str());
-this->minute = atoi(minuteString.c_str());
+int Time::addMinute(int minutes) {
+	changeTime(minutes);
+
+	return minute;
 }
 
-Time::Time( int day, int hour, int minute) {
-this->day = day;
-this->hour = hour;
-this->minute = minute;
+int Time::subDay(int days) {
+	changeTime( days * -1440);
+
+	return day;
 }
 
- int Time::addDay( int days) {
-day += days;
+int Time::subHour(int hours) {
+	changeTime( hours * -60 );
 
-return day;
+	return hour;
 }
 
- int Time::addHour( int hours) {
-hour += hours;
+int Time::subMinute(int minutes) {
+	changeTime( minutes * -1 );
 
-while(hour >= 24) {
-addDay(1);
-
-hour -= 24;
-}
-
-return hour;
-}
-
- int Time::addMinute( int minutes) {
-minute += minutes;
-
-while(minute >= 60) {
-addHour(1);
-
-minute -= 60;
-}
-
-return minute;
-}
-
- int Time::subDay( int days) {
-day -= days;
-
-return day;
-}
-
- int Time::subHour( int hours) {
-hour -= hours;
-
-while(hour < 0) {
-subDay(1);
-
-hour += 24;
-}
-
-return hour;
-}
-
- int Time::subMinute( int minutes) {
-minute -= minutes;
-
-while(minute < 0) {
-subHour(1);
-
-minute += 60;
-}
-
-return minute;
+	return minute;
 }
 
 string Time::getFormattedTime() {
-stringstream formattedTime;
+	stringstream formattedTime;
 
-formattedTime << "[" << day << "]";
-formattedTime << ((hour < 10) ? "0" : "") << hour << ":";
-formattedTime << ((minute < 10) ? "0" : "") << minute;
+	formattedTime << "[" << day << "]";
+	formattedTime << ((hour < 10) ? "0" : "") << hour << ":";
+	formattedTime << ((minute < 10) ? "0" : "") << minute;
 
-return formattedTime.str( );
+	return formattedTime.str();
 }
 
 int Time::getTimeInMinutes() {
-return minute + (hour + day * 24) * 60;
+	return minute + (hour + day * 24) * 60;
 }
 
 Time Time::operator+(Time other) {
-Time t(*this);
+	Time t(*this);
 
-t.addDay(other.getDay());
-t.addHour(other.getHour());
-t.addMinute(other.getMinute());
+	t.changeTime(other.getTimeInMinutes());
 
-return t;
+	return t;
 }
 
 Time Time::operator-(Time other) {
-Time t(*this);
+	Time t(*this);
 
-t.subDay(other.getDay());
-t.subHour(other.getHour());
-t.subMinute(other.getMinute());
+	t.changeTime(other.getTimeInMinutes() * -1);
 
-return t;
+	return t;
 }
 
 bool Time::operator==(Time other) {
-return this->getTimeInMinutes() == other.getTimeInMinutes();
+	return this->getTimeInMinutes() == other.getTimeInMinutes();
 }
 
 bool Time::operator>(Time other) {
-return this->getTimeInMinutes() > other.getTimeInMinutes();
+	return this->getTimeInMinutes() > other.getTimeInMinutes();
 }
 
 bool Time::operator<(Time other) {
-return this->getTimeInMinutes() < other.getTimeInMinutes();
+	return this->getTimeInMinutes() < other.getTimeInMinutes();
 }
 
 bool Time::operator>=(Time other) {
-return this->getTimeInMinutes() >= other.getTimeInMinutes();
+	return this->getTimeInMinutes() >= other.getTimeInMinutes();
 }
 
 bool Time::operator<=(Time other) {
-return this->getTimeInMinutes() <= other.getTimeInMinutes();
+	return this->getTimeInMinutes() <= other.getTimeInMinutes();
+}
+
+bool Time::changeTime(int p_minute) {
+	int time_in_minutes = getTimeInMinutes() + p_minute;
+
+	day = (time_in_minutes) / 1440;
+
+	time_in_minutes -= (time_in_minutes >= 0) ? day * 1440 : (day - 1) * 1440;
+
+	hour = time_in_minutes / 60;
+	minute = time_in_minutes - hour * 60;
+
+	return true;
 }
 
 /*void Time::operator=( Time& other ) {
-day = other.getDay( );
-hour = other.getHour( );
-minute = other.getMinute( );
-}*/
+ day = other.getDay( );
+ hour = other.getHour( );
+ minute = other.getMinute( );
+ }*/
--- src/Time.h
+++ src/Time.h
@@ -1,9 +1,9 @@
 /*
-* Time.h
-*
-* Created on: Oct 11, 2011
-* Author: SANDER
-*/
+ * Time.h
+ *
+ * Created on: Oct 11, 2011
+ * Author: SANDER
+ */
 
 #ifndef TIME_H_
 #define TIME_H_
@@ -12,150 +12,157 @@
 
 class Time {
 public:
-/**
-* Default Constructor
-*/
-Time();
-/**
-* Copy Constructor
-* @param c Time instance to copy
-*/
-Time(const Time & c);
-/**
-* Constructor with Formatted Time
-* @param timeFormatted a formatted time in format: HH:MM, or [DD]HH:MM
-*/
-Time(std::string timeFormatted);
-/**
-* Constructor with Time
-* @param day the day
-* @param hour the hour
-* @param minute the minute
-*/
-Time(int day, int hour, int minute);
+	/**
+	 * Default Constructor
+	 */
+	Time();
+	/**
+	 * Copy Constructor
+	 * @param c Time instance to copy
+	 */
+	Time(const Time & c);
+	/**
+	 * Constructor with Formatted Time
+	 * @param timeFormatted a formatted time in format: HH:MM, or [DD]HH:MM
+	 */
+	Time(std::string timeFormatted);
+	/**
+	 * Constructor with Time
+	 * @param day the day
+	 * @param hour the hour
+	 * @param minute the minute
+	 */
+	Time(int day, int hour, int minute);
 
-/**
-* Adds days to the Time
-* @param days the amount of days to add
-* @return the new day of the Time
-*/
-int addDay(int days);
-/**
-* Adds hours to the Time
-* @param hours the amount of hours to add
-* @return the new hour of the Time
-*/
-int addHour(int hours);
-/**
-* Adds minutes to the Time
-* @param minutes the amount of minutes to add
-* @return the new minute of the Time
-*/
-int addMinute(int minutes);
+	/**
+	 * Adds days to the Time
+	 * @param days the amount of days to add
+	 * @return the new day of the Time
+	 */
+	int addDay(int days);
+	/**
+	 * Adds hours to the Time
+	 * @param hours the amount of hours to add
+	 * @return the new hour of the Time
+	 */
+	int addHour(int hours);
+	/**
+	 * Adds minutes to the Time
+	 * @param minutes the amount of minutes to add
+	 * @return the new minute of the Time
+	 */
+	int addMinute(int minutes);
 
-/**
-* Subtracts days of the Time
-* @param days the amount of days to subtract
-* @return the new day of the Time
-*/
-int subDay(int days);
-/**
-* Subtracts hours of the Time
-* @param hours the amount of hours to subtract
-* @return the new hour of the Time
-*/
-int subHour(int hours);
-/**
-* Subtracts minutes of the Time
-* @param minutes the amount of minutes to subtract
-* @return the new minute of the Time
-*/
-int subMinute(int minutes);
+	/**
+	 * Subtracts days of the Time
+	 * @param days the amount of days to subtract
+	 * @return the new day of the Time
+	 */
+	int subDay(int days);
+	/**
+	 * Subtracts hours of the Time
+	 * @param hours the amount of hours to subtract
+	 * @return the new hour of the Time
+	 */
+	int subHour(int hours);
+	/**
+	 * Subtracts minutes of the Time
+	 * @param minutes the amount of minutes to subtract
+	 * @return the new minute of the Time
+	 */
+	int subMinute(int minutes);
 
-/**
-* Gets the Day
-*/
-int getDay() { return day; }
-/**
-* Gets the Hour
-*/
-int getHour() { return hour; }
-/**
-* Gets the Minute
-*/
-int getMinute() { return minute; }
+	/**
+	 * Gets the Day
+	 */
+	int getDay() {
+		return day;
+	}
+	/**
+	 * Gets the Hour
+	 */
+	int getHour() {
+		return hour;
+	}
+	/**
+	 * Gets the Minute
+	 */
+	int getMinute() {
+		return minute;
+	}
 
-/**
-* Returns a Time Formatted String
-* Format: [DD]HH:MM
-* @return a string with the formatted time
-*/
-std::string getFormattedTime();
+	/**
+	 * Returns a Time Formatted String
+	 * Format: [DD]HH:MM
+	 * @return a string with the formatted time
+	 */
+	std::string getFormattedTime();
 
-/**
-* Returns a int in minutes
-* Format: int
-* @return a int in minutes
-*/
-int getTimeInMinutes();
+	/**
+	 * Returns a int in minutes
+	 * Format: int
+	 * @return a int in minutes
+	 */
+	int getTimeInMinutes();
 
-/**
-* Operator +
-* Adds two times (this and other) and put it in a new one
-* @param other a reference to another Time object
-* @return a Time object with the two times added with eachother
-*/
-Time operator+(Time other);
-/**
-* Operator -
-* Subtracts two times (this and other) and put it in a new one
-* @param other a reference to another Time object
-* @return a Time object with the two times subtracted with eachother
-*/
-Time operator-(Time other);
+	/**
+	 * Operator +
+	 * Adds two times (this and other) and put it in a new one
+	 * @param other a reference to another Time object
+	 * @return a Time object with the two times added with eachother
+	 */
+	Time operator+(Time other);
+	/**
+	 * Operator -
+	 * Subtracts two times (this and other) and put it in a new one
+	 * @param other a reference to another Time object
+	 * @return a Time object with the two times subtracted with eachother
+	 */
+	Time operator-(Time other);
 
-/**
-* Operator ==
-* Compares if two Times are equal by their times
-* @param other a reference to another Time object
-* @return true if equals
-*/
-bool operator==(Time other);
-/**
-* Operator >
-* Compares if this Time is later than other Time
-* @param other a reference to another Time object
-* @return true if later
-*/
-bool operator>(Time other);
-/**
-* Operator <
-* Compares if this Time is earlier than other Time
-* @param other a reference to another Time object
-* @return true if earlier
-*/
-bool operator<(Time other);
-/**
-* Operator >=
-* Compares if this Time is later than or equal to other Time
-* @param other a reference to another Time object
-* @return true if later
-*/
-bool operator>=(Time other);
-/**
-* Operator <=
-* Compares if this Time is earlier than or equal to other Time
-* @param other a reference to another Time object
-* @return true if earlier
-*/
-bool operator<=(Time other);
+	/**
+	 * Operator ==
+	 * Compares if two Times are equal by their times
+	 * @param other a reference to another Time object
+	 * @return true if equals
+	 */
+	bool operator==(Time other);
+	/**
+	 * Operator >
+	 * Compares if this Time is later than other Time
+	 * @param other a reference to another Time object
+	 * @return true if later
+	 */
+	bool operator>(Time other);
+	/**
+	 * Operator <
+	 * Compares if this Time is earlier than other Time
+	 * @param other a reference to another Time object
+	 * @return true if earlier
+	 */
+	bool operator<(Time other);
+	/**
+	 * Operator >=
+	 * Compares if this Time is later than or equal to other Time
+	 * @param other a reference to another Time object
+	 * @return true if later
+	 */
+	bool operator>=(Time other);
+	/**
+	 * Operator <=
+	 * Compares if this Time is earlier than or equal to other Time
+	 * @param other a reference to another Time object
+	 * @return true if earlier
+	 */
+	bool operator<=(Time other);
 
 private:
-int day;
-int hour;
-int minute;
+	int day;
+	int hour;
+	int minute;
+
+	bool changeTime(int p_minute);
 
 };
-
 
 #endif /* TIME_H_ */
